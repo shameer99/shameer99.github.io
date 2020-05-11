ae4fed649851977f79ec9d071aa61675ed2b1d10
Muhammad Shameer Shahab <msshahab@umd.edu>
2020-05-11 02:30:53 -0400

    resume shows date

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function edit_text(){
+  var first_name_text = document.getElementById("first-name")
+  first_name_text.style.textAlign = null
+
+
+  // first_name_text.style.textAlign = "left"
+  // first_name_text.style.color = "red "
+}
